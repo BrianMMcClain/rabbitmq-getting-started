8b6c762b6c7dea9f60db737581be329776284046
Brian McClain <brianmmcclain@gmail.com>
2020-04-15 09:06:53 -0400

    Added comments

--- node/consumer.js
+++ node/consumer.js
@@ -2,16 +2,18 @@
 
 var amqp = require('amqplib/callback_api');
 
+// Connect to the rabbitmq server, channel, and queue
 amqp.connect('amqp://user:bitnami@localhost', function(error0, connection) {
   connection.createChannel(function(error1, channel) {
-    
     var queue = 'hello';
     channel.assertQueue(queue, {
       durable: false
     });
 
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    // Begin listening on the queue for any messages
     channel.consume(queue, function(msg) {
+      // When a message is received, log it to the console
       console.log(" > %s", msg.content.toString());
     }, {
       noAck: true
--- node/emitter.js
+++ node/emitter.js
@@ -4,28 +4,27 @@ const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-
 var amqp = require('amqplib/callback_api');
 
+// Recursive function to read user input for each message to send
 var readInput = function(channel, queue) {
     readline.question('> ', (msg) => {
+        // Send message to RabbitMQ
         channel.sendToQueue(queue, Buffer.from(msg));
         console.log("Message Sent: %s", msg);
         readInput(channel, queue);
     });
 }
 
+// Connect to the rabbitmq server, channel, and queue
 amqp.connect('amqp://user:bitnami@localhost', function(error0, connection) {
-  connection.createChannel(function(error1, channel) {
-    if (error1) {
-      throw error1;
-    }
+  connection.createChannel(function(_, channel) {
     var queue = 'hello';
     channel.assertQueue(queue, {
       durable: false
     });
 
+    // Begin reading user input
     readInput(channel, queue);
   });
 });
