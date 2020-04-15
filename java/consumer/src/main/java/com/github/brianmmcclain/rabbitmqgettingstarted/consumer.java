99935382b883450968970c7228623c9d04ad1db1
Brian McClain <brianmmcclain@gmail.com>
2020-04-15 09:56:01 -0400

    Add comments

--- java/consumer/src/main/java/com/github/brianmmcclain/rabbitmqgettingstarted/consumer.java
+++ java/consumer/src/main/java/com/github/brianmmcclain/rabbitmqgettingstarted/consumer.java
@@ -10,6 +10,8 @@ public class consumer {
 	private final static String QUEUE_NAME = "hello";
 
 	public static void main(String[] args) {
+		
+		// Connect to the RabbitMQ server
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
 		factory.setUsername("user");
@@ -17,10 +19,14 @@ public class consumer {
 
 		try {
 			Connection connection = factory.newConnection();
+
+			// Create a channel and connect it to the queue
 			Channel channel = connection.createChannel();
 			System.out.println("Waiting for messages. To exit press CTRL+C\n");
 
+			// Setup the callback to be invoked when a new message is received
 			DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+				// Read the message from the message object and print to stdout
 				String message = new String(delivery.getBody(), "UTF-8");
 				System.out.println(" > " + message);
 			};
--- java/emitter/src/main/java/com/github/brianmmcclain/rabbitmqgettingstarted/emitter.java
+++ java/emitter/src/main/java/com/github/brianmmcclain/rabbitmqgettingstarted/emitter.java
@@ -3,8 +3,6 @@ package com.github.brianmmcclain.rabbitmqgettingstarted;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 
-import java.io.IOException;
-
 import com.rabbitmq.client.Channel;
 
 public class emitter {
@@ -12,17 +10,24 @@ public class emitter {
 	private final static String QUEUE_NAME = "hello";
 
 	public static void main(String[] args) {
+		
+		// Connect to the RabbitMQ server
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
 		factory.setUsername("user");
 		factory.setPassword("bitnami");
 		try (Connection connection = factory.newConnection();
+			
+			// Create a channel and connect it to the queue
 			Channel channel = connection.createChannel()) {
 				channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-				
+			
+				// Begin reading user input
 				while (true) {
 					System.out.print("> ");
 					String message = System.console().readLine();
+					
+					// Send the message to the queue
 					channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
 					System.out.println("Sent '" + message + "'");
 				}
