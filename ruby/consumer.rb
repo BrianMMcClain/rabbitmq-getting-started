b8b8bdf76f5ec8f38886a0227b8f7cc904793123
Brian McClain <brianmmcclain@gmail.com>
2020-04-15 10:01:08 -0400

    Add comments

--- ruby/consumer.rb
+++ ruby/consumer.rb
@@ -1,12 +1,16 @@
 require 'bunny'
 
+# Set up the connection to the RabbitMQ server
 connection = Bunny.new(:username => "user", :password => "bitnami")
 connection.start
 
+# Set up the channel and queue in RabbitMQ
 channel = connection.create_channel
 queue = channel.queue('hello')
 
+# Subscribe on the queue and start listening for messages
 queue.subscribe do |_delivery_info, _properties, body|
+    # When a message is received, print to stdout
     puts " > Received Message: #{body}"
 end
 
@@ -14,4 +18,5 @@ puts "Subsribed to queue: #{queue.name}"
 puts "Press [Enter] to end"
 puts
 
+# End when user presses Enter
 gets
--- ruby/emitter.rb
+++ ruby/emitter.rb
@@ -1,15 +1,17 @@
 require 'bunny'
 
+# Set up the connection to the RabbitMQ server
 connection = Bunny.new(:username => "user", :password => "bitnami")
 connection.start
 
+# Set up the channel and queue in RabbitMQ
 channel = connection.create_channel
 queue = channel.queue('hello')
 
+# Start reading user input
 while true do
-    #message = "Hello at #{Time.now}"
-    message = gets.strip!
+    message = gets.strip! # Remove newline at end of message
+    # Send the message to RabbitMQ
     channel.default_exchange.publish(message, routing_key: queue.name)
     puts "Published \"#{message}\" on #{queue.name}"
-    sleep 1 
 end
