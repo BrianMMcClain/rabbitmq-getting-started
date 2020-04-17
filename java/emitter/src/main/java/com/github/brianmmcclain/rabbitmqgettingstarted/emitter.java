18d0261f7b6b12350bbec3c426babc55a555dd30
Brian McClain <brianmmcclain@gmail.com>
2020-04-17 09:15:58 -0400

    Update formatting, add more comments

--- java/emitter/src/main/java/com/github/brianmmcclain/rabbitmqgettingstarted/emitter.java
+++ java/emitter/src/main/java/com/github/brianmmcclain/rabbitmqgettingstarted/emitter.java
@@ -15,21 +15,29 @@ public class emitter {
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
 
-		try (Connection connection = factory.newConnection();
+		try {
+			Connection connection = factory.newConnection();
+				
+			// Create a channel
+			Channel channel = connection.createChannel();
 			
-			// Create a channel and connect it to the queue
-			Channel channel = connection.createChannel()) {
-				channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			// Create and connect to the queue. The arguments, on order, are:
+			// queue - Name of the queue we are connecting on
+			// durable - If true, RabbitMQ will write messages to disk
+			// exclusive - If true, only this connection may connect to the queue
+			// autoDelete - If true, the queue will be deleted when it is no longer in use
+			// The final argument takes a Map of additional optional arguments
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 			
-				// Begin reading user input
-				while (true) {
-					System.out.print("> ");
-					String message = System.console().readLine();
-					
-					// Send the message to the queue
-					channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-					System.out.println("Sent '" + message + "'");
-				}
+			// Begin reading user input
+			while (true) {
+				System.out.print("> ");
+				String message = System.console().readLine();
+				
+				// Send the message to the queue
+				channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+				System.out.println("Sent '" + message + "'");
+			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
